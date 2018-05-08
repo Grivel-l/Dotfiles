bf1a0de644921dc9bc5d8c44abed21805273d697
Léo <legrivel@student.le-101.fr>
2018-05-08 03:47:22 +0200

    Update

--- lockscreen/lockscreen.sh
+++ lockscreen/lockscreen.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DIR=~/.config/lockscreen
+light -S 0
 betterlockscreen -u <(import -silent -window root png:- | mogrify -blur 0x8 png:- | composite -gravity South -geometry -20x1200 $DIR/foreground.png png:- png:-)
 rm -f $DIR/lockscreen.png
 betterlockscreen --lock
+light -S 100
 
