d7b6392397c0894b0218080f149ccc7c1465f73e
Léo <legrivel@student.le-101.fr>
2018-05-08 03:39:52 +0200

    Update

--- lockscreen/lockscreen.sh
+++ lockscreen/lockscreen.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 DIR=~/.config/lockscreen
+betterlockscreen -u <(import -silent -window root png:- | mogrify -blur 0x8 png:- | composite -gravity South -geometry -20x1200 $DIR/foreground.png png:- png:-)
 rm -f $DIR/lockscreen.png
-flameshot full -p $DIR
-sleep 1
-convert -blur 0x8 $DIR/lockscreen.png $DIR/lockscreen2.png
-convert -composite $DIR/lockscreen2.png $DIR/foreground.png -gravity South -geometry -20x1200 $DIR/lockscreen.png
-rm -f $DIR/lockscreen2.png
-betterlockscreen -u $DIR/lockscreen.png
 betterlockscreen --lock
 
