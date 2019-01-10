592859538b0fc61db4e00fac49d9001d4b849496
Léo <legrivel@student.le-101.fr>
2019-01-10 02:15:12 +0100

    Can't execute command after lock since last update

--- config/lockscreen/lockscreen.sh
+++ config/lockscreen/lockscreen.sh
@@ -3,6 +3,6 @@ DIR=~/.config/lockscreen
 light -S 0
 betterlockscreen -u <(import -silent -window root png:- | mogrify -blur 0x8 png:- | composite -gravity South -geometry -20x1200 $DIR/foreground.png png:- png:-)
 rm -f $DIR/lockscreen.png
-betterlockscreen --lock
 light -S 100
+betterlockscreen --lock
 
