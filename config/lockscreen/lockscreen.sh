60dbc2f7877d8a99f6e79f4f96d98f655810b30c
Léo <legrivel@student.le-101.fr>
2020-01-06 02:13:51 +0100

    Set lockscreen in background

--- config/lockscreen/lockscreen.sh
+++ config/lockscreen/lockscreen.sh
@@ -4,5 +4,5 @@ light -S 0
 betterlockscreen -u <(import -silent -window root png:- | mogrify -blur 0x8 png:- | composite -gravity South -geometry -20x1200 $DIR/foreground.png png:- png:-)
 rm -f $DIR/lockscreen.png
 light -S 100
-betterlockscreen --lock
+betterlockscreen --lock &
 
