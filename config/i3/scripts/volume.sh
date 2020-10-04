0bba64797b50c10f5e170e0f1a165da8042c14e8
Léo <legrivel@student.le-101.fr>
2020-10-05 01:10:35 +0200

    Call volume script in i3 bindings

--- config/i3/scripts/volume.sh
+++ config/i3/scripts/volume.sh
@@ -12,10 +12,10 @@ sink=`pactl list sinks | grep -B 29 "priority: $lowest" | grep "Sink #" | cut -d
 if [ "$1" = "up" ];
 then
     echo $sink
-    pactl set-sink-volume $sink +3%
+    pactl set-sink-volume $sink +2%
 elif [ "$1" = "down" ];
 then
-    pactl set-sink-volume $sink -3%
+    pactl set-sink-volume $sink -2%
 elif [ "$1" = "mute" ];
 then
     pactl set-sink-mute $sink toggle
