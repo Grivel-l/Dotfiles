06e3dd812460c48876f568b5eac5a3dc95c396f8
Grivel-l <legrivel@student.le-101.fr>
2020-10-13 00:55:06 +0200

    Get volume by running sink

--- config/i3/scripts/volume.sh
+++ config/i3/scripts/volume.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-priority=`pactl list sinks | grep priority | cut -d ":" -f 3 | grep -v "not available" | cut -d "," -f 1 | cut -d ")" -f 1 | xargs`
-lowest=`echo $priority | cut -d " " -f 1`
-for nbr in $priority;
-do
-  if [ $nbr -lt $lowest ];
-  then
-    lowest=$nbr;
-  fi
-done
-sink=`pactl list sinks | grep -e "Sink" -e "priority: $lowest" | grep -B 1 "priority: $lowest" | grep "Sink #" | cut -d "#" -f 2`
+sink=`pactl list sinks short | grep "RUNNING"`
+if [ -z "$sink" ];
+then
+    sink=`pactl list sinks short | grep "IDLE"`
+fi;
+sink=`echo $sink | head -c 1`
 if [ "$1" = "up" ];
 then
     pactl set-sink-volume $sink +2%
