5838a4af80752b34cdf9eba7de82e8a8eaa95cab
Grivel-l <legrivel@student.le-101.fr>
2020-11-16 17:21:40 +0100

    Correctly create background before setting it in cache

--- config/lockscreen/lockscreen.sh
+++ config/lockscreen/lockscreen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DIR=~/.config/lockscreen
 light -S 0
-betterlockscreen -u <(import -silent -window root png:- | mogrify -blur 0x8 png:- | composite -gravity South -geometry -20x1200 $DIR/foreground.png png:- png:-)
-rm -f $DIR/lockscreen.png
+import -silent -window root png:- | mogrify -blur 0x8 png:- | composite -gravity South -geometry -20x1200 $DIR/foreground.png png:- png:- > $DIR/background
+betterlockscreen -u $DIR/background
+rm $DIR/background
 light -S 100
 betterlockscreen --lock &
-
